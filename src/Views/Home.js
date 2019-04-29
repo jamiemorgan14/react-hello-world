ad350af2dfa4cace9a386f50a7b11e962826058a
Jamie <jmorgan3475@gmail.com>
2019-04-29 09:43:01 -0600

    meh

---
+++ src/Views/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  const [counter, setCounter] = 0
+  return
+  <button className="btn btn-danger btn-large">{counter}</button>
+
+}
+
+
+
+
+export default Home;
