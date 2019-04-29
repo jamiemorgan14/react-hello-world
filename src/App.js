2de4e4406f6b2958e5c4b81c22aaa144aa534e0c
Jamie <jmorgan3475@gmail.com>
2019-04-28 19:01:12 -0600

    setup

--- src/App.js
+++ src/App.js
@@ -6,19 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         Suck it nerd
-           
-        </a>
+        <button>number</button>
       </header>
     </div>
   );
